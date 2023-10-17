1205587930c8444c19a17996336f52a2bfc42fcd
Felix Pinzon Gaona <flp1901pg@gmail.com>
2023-10-17 15:12:30 -0400

    Added main.js to html file and removed the duplicate script from html file.
    Signed-off-by: Felix Pinzon Gaona <flp1901pg@gmail.com>

--- main.js
+++ main.js
@@ -1,86 +1,105 @@
-const { ipcRenderer } = require('electron');
-const Chart = require('chart.js'); // Import Chart.js
-
 const teamNameInput = document.getElementById('teamNameInput');
-const initialWeightInput = document.getElementById('initialWeightInput');
-const addTeamButton = document.getElementById('addTeamButton');
-const teamTable = document.getElementById('teamTable');
-const chartCanvas = document.getElementById('teamChart'); // Reference the canvas element
-
-// creates a class for teams
-class Team {
-	name = "unnamed team";
-	initialWeight = 0;
-	currentScore = 0;
+    const initialWeightInput = document.getElementById('initialWeightInput');
+    const addTeamButton = document.getElementById('addTeamButton');
+    const teamTableBody = document.getElementById('teamTableBody');
+    const teamChart = document.getElementById('teamChart').getContext('2d'); // Get the chart context
   
-	breakpoint = false;
-	constructor(name, initialWeight){
-        this.name = name;
-        this.initialWeight = initialWeight;
+    // Array to store teams
+    const teams = [];
+  
+    // Create an initial empty chart
+    let chart;
+  
+    // Function to add a team
+    function addTeam() {
+      const name = teamNameInput.value;
+      const initialWeight = parseFloat(initialWeightInput.value);
+  
+      if (name && !isNaN(initialWeight)) {
+        const team = {
+          name,
+          initialWeight,
+          currentScore: 0,
+          breakPoint: false
+        };
+  
+        teams.push(team);
+        updateTeamTable();
+        updateChart();
+        teamNameInput.value = '';
+        initialWeightInput.value = '';
+      }
     }
-}
-
-// Array to store the list of teams
-const teams = [];
-
-// Function to add a team to the list
-function addTeam(name, initialWeight) {
-  var addedTeam = new Team(name, initialWeight)
-  teams.push(addedTeam);
-  updateTeamTable();
-  updateChart(); // Call the function to update the chart
-}
-
-// Function to update the team table on the screen
-function updateTeamTable() {
-  // ... (existing code for updating the team table) ...
-}
-
-// Function to update the Chart.js chart
-function updateChart() {
-  // Extract the team names and scores for the chart
-  const labels = teams.map((team) => team.name);
-  const scores = teams.map((team) => team.currentScore);
-
-  // Create a new Chart.js chart
-  const ctx = chartCanvas.getContext('2d');
-  const chart = new Chart(ctx, {
-    type: 'bar', // You can choose the chart type (e.g., bar, line, pie, etc.)
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: 'Scores',
-          data: scores,
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
+  
+    // Function to update the team table
+    function updateTeamTable() {
+      teamTableBody.innerHTML = '';
+      teams.forEach((team, index) => {
+        const row = teamTableBody.insertRow();
+        row.innerHTML = `
+          <td>${team.name}</td>
+          <td>${team.initialWeight}</td>
+          <td>${team.currentScore}</td>
+          <td>${team.breakPoint ? 'Broken' : 'Not Broken'}</td>
+          <td>
+            <button onclick="updateScore(${index})">Update Score</button>
+            <button onclick="markAsBroken(${index})">Mark as Broken</button>
+          </td>
+        `;
+      });
+    }
+  
+    // Function to update the Chart.js chart
+    function updateChart() {
+      if (chart) {
+        chart.destroy(); // Destroy the previous chart instance to prevent memory leaks
+      }
+  
+      // Extract the team names and scores for the chart
+      const labels = teams.map((team) => team.name);
+      const scores = teams.map((team) => team.currentScore);
+  
+      // Create a new Chart.js chart
+      chart = new Chart(teamChart, {
+        type: 'bar', // You can choose the chart type (e.g., bar, line, pie, etc.)
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Scores',
+              data: scores,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1,
+            },
+          ],
         },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
         },
-      },
-    },
-  });
-}
-
-// Event listener for the "Add Team" button
-addTeamButton.addEventListener('click', () => {
-  const name = teamNameInput.value.trim();
-  const initialWeight = parseFloat(initialWeightInput.value.trim());
-
-  if (name && !isNaN(initialWeight)) {
-    addTeam(name, initialWeight);
-    teamNameInput.value = '';
-    initialWeightInput.value = '';
-  }
-});
-
-// Listen for IPC messages from the main process (if needed)
-ipcRenderer.on('some-message-from-main', (event, data) => {
-  // Handle messages from the main process if necessary
-});
+      });
+    }
+  
+    // Function to update the score of a team
+    function updateScore(index) {
+      const addedWeight = parseFloat(prompt(`Enter weight added for ${teams[index].name}:`));
+      if (!isNaN(addedWeight)) {
+        teams[index].currentScore += addedWeight;
+        updateTeamTable();
+        updateChart();
+      }
+    }
+  
+    // Function to mark a team as broken
+    function markAsBroken(index) {
+      teams[index].breakPoint = true;
+      updateTeamTable();
+      updateChart();
+    }
+  
+    // Event listener for the "Add Team" button
+    addTeamButton.addEventListener('click', addTeam);
