9cd7e821234667bdbbbd7553dd83cb3b31c4ebd0
Ethelyn <metallicminiboss@gmail.com>
2023-10-29 21:12:03 -0400

    Added scoring function, scores now update properly. Will check if scores for teams not broken should do the weird thing at the end of the word doc or not.
    Signed-off-by: Ethelyn <metallicminiboss@gmail.com>

--- main.js
+++ main.js
@@ -1,146 +1,198 @@
 const teamNameInput = document.getElementById('teamNameInput');
-    const bridgeWeightInput = document.getElementById('bridgeWeightInput');
-    const addTeamButton = document.getElementById('addTeamButton');
-    const teamTableBody = document.getElementById('teamTableBody');
-    const teamChart = document.getElementById('teamChart').getContext('2d'); // Get the chart context
-    const loadAllButton = document.getElementById('loadAllButton');
-    const loadAllInput = document.getElementById('loadAllInput');
+const bridgeWeightInput = document.getElementById('bridgeWeightInput');
+const addTeamButton = document.getElementById('addTeamButton');
+const teamTableBody = document.getElementById('teamTableBody');
+const teamChart = document.getElementById('teamChart').getContext('2d'); // Get the chart context
+const loadAllButton = document.getElementById('loadAllButton');
+const loadAllInput = document.getElementById('loadAllInput');
   
-    // Array to store teams
-    const teams = [];
+// Array to store teams
+const teams = [];
   
-    // Create an initial empty chart
-    let chart;
+// Create an initial empty chart
+let chart;
     
-    // Class for teams. Each team will be an instance of this. This will allow us to keep track of each team's values more easily.
-    class Team {
-        constructor(name, bridgeWeight){
-            this.name = name;
-            this.bridgeWeight = bridgeWeight;
-        }
-        currentScore = 0;
-        breakPoint = false;
-        load = 0;
-        bDEF = 10;
-
+// Class for teams. Each team will be an instance of this. This will allow us to keep track of each team's values more easily.
+class Team {
+    constructor(name, bridgeWeight){
+        this.name = name;
+        this.bridgeWeight = bridgeWeight;
     }
+    score = 0;
+    breakPoint = false;
+    load = 0;
+    mdr = 0;
+    bDEF = 10;
+
+}
 
 
-    // Function to add a team
-    function addTeam() {
-      const name = teamNameInput.value;
-      const bridgeWeight = parseFloat(bridgeWeightInput.value);
+// Function to add a team
+function addTeam() {
+    const name = teamNameInput.value;
+    const bridgeWeight = parseFloat(bridgeWeightInput.value);
   
-      if (name && !isNaN(bridgeWeight)) {
-        const team = new Team(name, bridgeWeight);
+    if (name && !isNaN(bridgeWeight)) {
+    const team = new Team(name, bridgeWeight);
   
-        teams.push(team);
-        updateTeamTable();
-        updateChart();
-        teamNameInput.value = '';
-        bridgeWeightInput.value = '';
+    teams.push(team);
+    updateTeamTable();
+    updateChart();
+    teamNameInput.value = '';
+    bridgeWeightInput.value = '';
 
-      }
-    }
-  
-    // Function to update the team table
-    function updateTeamTable() {
-      teamTableBody.innerHTML = '';
-      teams.forEach((team, index) => {
-        const row = teamTableBody.insertRow();
-        row.innerHTML = `
-          <td>${team.name}</td>
-          <td>${team.bridgeWeight}</td>
-          <td>${team.currentScore}</td>
-          <td>${team.load}</td>
-          <td>${team.breakPoint ? 'Broken' : 'Not Broken'}</td>
-          <td>${team.bDEF}</td>
-          <td>
-            <button onclick="updateLoad(${index})">Update Load</button>
-            <button onclick="updateBDEF(${index})">Update BDEF</button>
-            <button onclick="markAsBroken(${index})">Mark as Broken</button>
-          </td>
-        `;
-      });
-    }
-  
-    // Function to update the Chart.js chart
-    function updateChart() {
-      if (chart) {
-        chart.destroy(); // Destroy the previous chart instance to prevent memory leaks
-      }
-  
-      // Extract the team names and scores for the chart
-      const labels = teams.map((team) => team.name);
-      const scores = teams.map((team) => team.currentScore);
-  
-      // Create a new Chart.js chart
-      chart = new Chart(teamChart, {
-        type: 'bar', // You can choose the chart type (e.g., bar, line, pie, etc.)
-        data: {
-          labels: labels,
-          datasets: [
-            {
-              label: 'Scores',
-              data: scores,
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
-              borderColor: 'rgba(75, 192, 192, 1)',
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
-    }
-  
-    // Function to update the score of a team
-    function updateLoad(index) {
-      const addedWeight = parseFloat(prompt(`Enter load change for ${teams[index].name}:`));
-      if (!isNaN(addedWeight)) {
-        teams[index].load += addedWeight;
-        updateTeamTable();
-        updateChart();
-      }
-    }
-
-function updateBDEF(index) {
-    const bDEF = parseFloat(prompt(`Enter BDEF for ${teams[index].name}:`));
-    if (!isNaN(bDEF)) {
-        teams[index].bDEF = bDEF;
-        updateTeamTable();
-        updateChart();
     }
 }
-    // function to update load of all teams
-    function loadAll() {
-        
-        if ((teams.length != 0) && (!isNaN(loadAllInput.valueAsNumber))){
 
-            load = loadAllInput.valueAsNumber;
-            for (i = 0; i < teams.length; i++){
-                teams[i].load += load;
-            }
-            updateTeamTable();
-            updateChart();
+// Function to calculate all teams' scores
+function calculateScores() {
+    // set up variables for maxmimum/minimum values
+    let [bwMax, lpMax, mdrMax, bwMin, lpMin, mdrMin] = [0, 0, 0, 0, 0, 0];
+
+    // loop through teams once to determine maxes/mins
+    for (let i = 0; i < teams.length; i++){
+        currentTeam = teams[i];
+        if (currentTeam.bridgeWeight > bwMax){
+            bwMax = currentTeam.bridgeWeight;
         }
-        else {
-            console.log("Cannot add value to teams.");
+        if (currentTeam.bridgeWeight < bwMin){
+            bwMin = currentTeam.bridgeWeight;
+        }
+
+
+        if (currentTeam.load > lpMax){
+            lpMax = currentTeam.load;
+        }
+        if (currentTeam.load < lpMin){
+            lpMin = currentTeam.load;
+        }
+
+
+        currentTeam.mdr = currentTeam.bridgeWeight/currentTeam.load;
+        if (currentTeam.mdr > mdrMax) {
+            mdrMax = currentTeam.mdr;
+        }
+        if (currentTeam.mdr < mdrMin) {
+            mdrMin = currentTeam.mdr;
         }
     }
-    // Function to mark a team as broken
-    function markAsBroken(index) {
-      teams[index].breakPoint = !teams[index].breakPoint;
-      updateTeamTable();
-      updateChart();
+        
+    // loop through teams again to calculate their scores
+    for (let i = 0; i < teams.length; i++) {
+        let currentTeam = teams[i];
+        let bwr = 1 + (49/(bwMax - bwMin) * (bwMax - currentTeam.bridgeWeight));
+        let lpr = 1 + (49/(lpMax - lpMin) * (currentTeam.load - lpMin));
+        let mdrr = 1 + (49/(mdrMax - mdrMin) * (mdrMax - currentTeam.mdr));
+
+        let score = currentTeam.bDEF + bwr + lpr + mdrr;
+        currentTeam.score = currentTeam.bDEF + bwr + lpr + mdrr;
+    }
+
+
+}
+  
+// Function to update the team table
+function updateTeamTable() {
+    teamTableBody.innerHTML = '';
+    teams.forEach((team, index) => {
+    const row = teamTableBody.insertRow();
+    row.innerHTML = `
+        <td>${team.name}</td>
+        <td>${team.bridgeWeight}</td>
+        <td>${team.score}</td>
+        <td>${team.load}</td>
+        <td>${team.breakPoint ? 'Broken' : 'Not Broken'}</td>
+        <td>${team.bDEF}</td>
+        <td>
+        <button onclick="updateLoad(${index})">Update Load</button>
+        <button onclick="updateBDEF(${index})">Update BDEF</button>
+        <button onclick="markAsBroken(${index})">Mark as Broken</button>
+        </td>
+    `;
+    });
+}
+  
+// Function to update the Chart.js chart
+function updateChart() {
+    if (chart) {
+    chart.destroy(); // Destroy the previous chart instance to prevent memory leaks
     }
   
-    // Event listener for the "Add Team" button
-    addTeamButton.addEventListener('click', addTeam);
+    // Extract the team names and scores for the chart
+    const labels = teams.map((team) => team.name);
+    const scores = teams.map((team) => team.score);
+  
+    // Create a new Chart.js chart
+    chart = new Chart(teamChart, {
+    type: 'bar', // You can choose the chart type (e.g., bar, line, pie, etc.)
+    data: {
+        labels: labels,
+        datasets: [
+        {
+            label: 'Scores',
+            data: scores,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+        },
+        ],
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true,
+        },
+        },
+    },
+    });
+}
+  
+// Function to update the score of a team
+function updateLoad(index) {
+    const addedWeight = parseFloat(prompt(`Enter load change for ${teams[index].name}:`));
+    if (!isNaN(addedWeight)) {
+    teams[index].load += addedWeight;
+    calculateScores();
+    updateTeamTable();
+    updateChart();
+    }
+}
 
-    loadAllButton.addEventListener('click', loadAll);
+function updateBDEF(index) {
+const bDEF = parseFloat(prompt(`Enter BDEF for ${teams[index].name}:`));
+if (!isNaN(bDEF)) {
+    teams[index].bDEF = bDEF;
+    updateTeamTable();
+    updateChart();
+}
+}
+// function to update load of all teams
+function loadAll() {
+        
+    if ((teams.length != 0) && (!isNaN(loadAllInput.valueAsNumber))){
+
+        load = loadAllInput.valueAsNumber;
+        for (i = 0; i < teams.length; i++){
+            if (!teams[i].breakPoint) {
+                teams[i].load += load;
+            }
+        }
+        calculateScores();
+        updateTeamTable();
+        updateChart();
+    }
+    else {
+        console.log("Cannot add value to teams.");
+    }
+}
+// Function to mark a team as broken
+function markAsBroken(index) {
+    teams[index].breakPoint = !teams[index].breakPoint;
+    updateTeamTable();
+    updateChart();
+}
+  
+// Event listener for the "Add Team" button
+addTeamButton.addEventListener('click', addTeam);
+
+loadAllButton.addEventListener('click', loadAll);
